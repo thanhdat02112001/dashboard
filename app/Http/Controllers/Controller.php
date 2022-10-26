6a7d24640e1b0851f101c29a4ae2547426f37a6f
Datdt <thanhdat02112001pt@gmail.com>
2022-10-26 18:24:57 +0700

    update first 4 chart

--- app/Http/Controllers/Controller.php
+++ app/Http/Controllers/Controller.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\ReportTransaction;
 use Illuminate\Foundation\Auth\Access\AuthorizesRequests;
 use Illuminate\Foundation\Bus\DispatchesJobs;
 use Illuminate\Foundation\Validation\ValidatesRequests;
@@ -10,4 +11,9 @@ use Illuminate\Routing\Controller as BaseController;
 class Controller extends BaseController
 {
     use AuthorizesRequests, DispatchesJobs, ValidatesRequests;
+    protected $successTrans;
+    public function __construct()
+    {
+        $this->successTrans =  ReportTransaction::where('trans_status', 5)->orderBy('dates', 'ASC')->get();
+    }
 }
